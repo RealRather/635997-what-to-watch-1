6c6f168d032f08763b0372c73e26850669139d7b
romia <irinapushaa@gmail.com>
2019-05-15 19:24:45 +0300

    Fixes tests

--- src/components/movie-card/movie-card.e2e.test.js
+++ src/components/movie-card/movie-card.e2e.test.js
@@ -4,21 +4,23 @@ import Adapter from 'enzyme-adapter-react-16';
 import MovieCard from './movie-card.jsx';
 
 Enzyme.configure({adapter: new Adapter()});
+const mock = {
+  id: `33`,
+  name: `Bohemian`,
+  src: `img/bohemian.jpg`
+};
 
 it(`Simulate click event in MovieCard`, () =>{
+  const movieName = mock;
   const clickHandler = jest.fn();
   const movie = shallow(<MovieCard
-    movieName={
-      {
-        filmName: `Bohemian`,
-        src: `img/bohemian.jpg`
-      }
-    }
+    movieName={movieName}
     onClick = {clickHandler}
   />);
-
   const cardTitle = movie.find(`.small-movie-card__title`);
+  const playButton = movie.find(`.small-movie-card__play-btn`);
   cardTitle.simulate(`click`, {preventDefault() {}});
-
   expect(clickHandler).toHaveBeenCalledTimes(1);
+  playButton.simulate(`click`, {targetInst: movieName.id});
+  expect(clickHandler).toHaveBeenCalledWith({targetInst: movieName.id});
 });
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -8,7 +8,7 @@ const MovieCard = ((props) => {
     _onFocusHandler
   } = props;
   return <article className="small-movie-card catalog__movies-card" onFocus={_onFocusHandler}>
-    <button className="small-movie-card__play-btn" type="button">Play</button>
+    <button className="small-movie-card__play-btn" type="button" onClick={onClick}>Play</button>
     <div className="small-movie-card__image">
       <img src={`${movieName.src}`} alt={`${movieName.name}`} width="280" height="175"/>
     </div>
