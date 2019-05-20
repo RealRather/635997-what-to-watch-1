9cb06e3b67d19e5d96ba7aa19758279e7131592d
romia <irinapushaa@gmail.com>
2019-05-20 14:23:45 +0300

    Adds previews in mocks/films.js

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -2,28 +2,47 @@ const films = [
   {
     id: `1`,
     name: `Fantastic Beasts: The Crimes of Grindelwald`,
-    src: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+    src: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    preview: [
+      `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+      `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
+    ],
   },
   {
     id: `2`,
     name: `Bohemian Rhapsody`,
-    src: `img/bohemian-rhapsody.jpg`
+    src: `img/bohemian-rhapsody.jpg`,
+    preview: [
+      `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+      `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
+    ],
   },
   {
     id: `3`,
     name: `Macbeth`,
-    src: `img/macbeth.jpg`
+    src: `img/macbeth.jpg`,
+    preview: [
+      `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+      `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
+    ],
   },
   {
     id: `4`,
     name: `We need to talk about Kevin`,
-    src: `img/we-need-to-talk-about-kevin.jpg`
-
+    src: `img/we-need-to-talk-about-kevin.jpg`,
+    preview: [
+      `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+      `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
+    ],
   },
   {
     id: `5`,
     name: `Johnny English`,
-    src: `img/johnny-english.jpg`
+    src: `img/johnny-english.jpg`,
+    preview: [
+      `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+      `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
+    ],
   }
 ];
 
